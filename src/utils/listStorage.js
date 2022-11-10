61e2b94e9b04bc11bd91ebc1b337a0aec02164aa
azhechkova <alinazhech@gmail.com>
2022-11-10 23:46:57 +0200

    Fixed bug with local storage

--- src/utils/listStorage.js
+++ src/utils/listStorage.js
@@ -1,15 +1,12 @@
 export const saveListToStorage = (list) => {
-    localStorage.setItem('watchList', JSON.stringify(list))
+    localStorage.setItem('watchList', JSON.stringify(list || []))
 }
 
 export const getListFromStorage = (list) => {
-    if (localStorage.getItem('watchList') === null) {
-        localStorage.setItem('watchList', JSON.stringify([]))
+    if (!localStorage.getItem('watchList')) {
+        saveListToStorage(list || [])
     } else {
-        const watchlistLocal = localStorage.getItem(
-            'watchList',
-            JSON.stringify(list)
-        )
+        const watchlistLocal = localStorage.getItem('watchList')
 
         return JSON.parse(watchlistLocal)
     }
